db5555897f7cc5219ad4bdb5568551f3f2da6930
unknown <jorgegrandegonzalez95@gmail.com>
2020-05-16 00:25:50 +0200

    p4--

--- P4/PracticaP4/video.js
+++ P4/PracticaP4/video.js
@@ -4,6 +4,7 @@ console.log("Ejecutando JS...");
 const video1 = document.getElementById("video1")
 const video2 = document.getElementById("video2")
 const video3 = document.getElementById("video3")
+const imagenfija = document.getElementById("imagenfija")
 const pantalla_general = document.getElementById("pantalla_general")
 
 //-- Imagen estática a mostrar cuando el video no
@@ -11,10 +12,10 @@ const pantalla_general = document.getElementById("pantalla_general")
 pantalla_general.poster="https://github.com/myTeachingURJC/2019-2020-CSAAI/raw/master/L10/test.png";
 
 //-- Obtener los botones
-const boton1 = document.getElementById("boton1")
-const boton2 = document.getElementById("boton2")
-const boton3 = document.getElementById("boton3")
-var condicion = false;
+const boton1 = document.getElementById("boton1");
+const boton2 = document.getElementById("boton2");
+const boton3 = document.getElementById("boton3");
+const boton4 = document.getElementById("boton4");
 
 //-- Función de retrollamada del botón de ver
 boton1.onclick = () => {
@@ -29,6 +30,7 @@ boton1.onclick = () => {
   video1.style.border = '4px solid green';
   video2.style.border = '4px solid black';
   video3.style.border = '4px solid black';
+  imagenfija.style.border = '4px solid black';
 
   pantalla_general.play();
 };
@@ -45,6 +47,7 @@ boton2.onclick = () => {
   video1.style.border = '4px solid black';
   video2.style.border = '4px solid green';
   video3.style.border = '4px solid black';
+  imagenfija.style.border = '4px solid black';
 
   pantalla_general.play();
 };
@@ -61,18 +64,98 @@ boton3.onclick = () => {
   video1.style.border = '4px solid black';
   video2.style.border = '4px solid black';
   video3.style.border = '4px solid green';
-  condicion = false;
+  imagenfija.style.border = '4px solid black';
 
   pantalla_general.play();
 };
 
 // Mejoras
+boton4.onclick = () => {
+  console.log("Video4");
+  pantalla_general.src = null;
+  pantalla_general.poster = imagenfija.src;
+
+  video1.muted = true;
+  video2.muted = true;
+  video3.muted = true;
+
+  video1.style.border = '4px solid black';
+  video2.style.border = '4px solid black';
+  video3.style.border = '4px solid black';
+  imagenfija.style.border = '4px solid green';
+};
+
 const automatico = document.getElementById("automatico");
+const normal = document.getElementById("normal");
+var condicion = false;
+var init;
+var finish;
+var count;
 
 automatico.onclick = () => {
+  console.log('Modo bucle');
+  automatico.style.border = '5px solid green';
+  normal.style.border = '5px solid black';
+  init = pantalla_general.currentTime;
+  finish = init + 3;
   condicion = true;
-  while (condicion) {
-    console.log("hola");
-    wait(3000);
+  video1.muted = true;
+  video2.muted = true;
+  video3.muted = true;
+
+  if (video1.style.border == '4px solid green'){
+    count = 0;
   }
+  if (video2.style.border == '4px solid green'){
+    count = 1;
+  }
+  if (video3.style.border == '4px solid green'){
+    count = 2;
+  }
+}
+
+setInterval(()=>{
+  if(condicion){
+    if (count > 2) {
+      count = 0;
+    }
+    if (pantalla_general.currentTime > finish){
+        if (count == 0) {
+          pantalla_general.src= video1.src;
+          pantalla_general.currentTime = video1.currentTime;
+        }else if (count == 1) {
+          pantalla_general.src= video2.src;
+          pantalla_general.currentTime = video2.currentTime;
+        }else {
+          pantalla_general.src= video3.src;
+          pantalla_general.currentTime = video3.currentTime;
+        }
+        pantalla_general.play();
+        pantalla_general.currentTime = init;
+        count = count + 1;
+    }
+    console.log(count);
+  }
+},3000);
+
+normal.onclick = () => {
+  console.log('Modo normal');
+  condicion = false;
+  automatico.style.border = '4px solid black';
+  normal.style.border = '4px solid green';
+
+  if (video1.style.border == '4px solid green'){
+    pantalla_general.src= video1.src;
+    pantalla_general.currentTime = video3.currentTime;
+
+  }
+  if (video2.style.border == '4px solid green'){
+    pantalla_general.src= video2.src;
+    pantalla_general.currentTime = video2.currentTime;
+  }
+  if (video3.style.border == '4px solid green'){
+    pantalla_general.src= video3.src;
+    pantalla_general.currentTime = video3.currentTime;
+  }
+  pantalla_general.play();
 }
