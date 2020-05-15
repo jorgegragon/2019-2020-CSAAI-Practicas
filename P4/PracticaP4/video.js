5b54b259ed3ed15cc06b570bb6d4c9e4713056eb
unknown <jorgegrandegonzalez95@gmail.com>
2020-05-15 21:54:52 +0200

    p4 avances

--- P4/PracticaP4/video.js
+++ P4/PracticaP4/video.js
@@ -2,29 +2,77 @@ console.log("Ejecutando JS...");
 
 //----- Obtener elemento de video y configurarlo
 const video1 = document.getElementById("video1")
-video1.width=200;  //-- Tamaño de la pantalla de video
-video1.height=100;
+const video2 = document.getElementById("video2")
+const video3 = document.getElementById("video3")
+const pantalla_general = document.getElementById("pantalla_general")
 
 //-- Imagen estática a mostrar cuando el video no
 //-- ha arrancado
-video1.poster="https://github.com/myTeachingURJC/2019-2020-CSAAI/raw/master/L10/test.png";
+pantalla_general.poster="https://github.com/myTeachingURJC/2019-2020-CSAAI/raw/master/L10/test.png";
 
 //-- Obtener los botones
-const play = document.getElementById("play")
-const stop = document.getElementById("stop")
+const boton1 = document.getElementById("boton1")
+const boton2 = document.getElementById("boton2")
+const boton3 = document.getElementById("boton3")
+var condicion = false;
 
 //-- Función de retrollamada del botón de ver
-play.onclick = () => {
-  console.log("Click!");
-  video1.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4"
-  video1.play();
+boton1.onclick = () => {
+  console.log("Video1");
+  pantalla_general.src= video1.src;
+  pantalla_general.currentTime = video1.currentTime;
+
+  video1.muted = false;
+  video2.muted = true;
+  video3.muted = true;
+
+  video1.style.border = '4px solid green';
+  video2.style.border = '4px solid black';
+  video3.style.border = '4px solid black';
+
+  pantalla_general.play();
 };
 
-//-- Funcion de retrollamada del boton de parar
-stop.onclick = () => {
-  video1.pause();
+boton2.onclick = () => {
+  console.log("Video2");
+  pantalla_general.src= video2.src;
+  pantalla_general.currentTime = video2.currentTime;
 
-  //-- Quitar la fuente de video, para que se muestre la
-  //-- imagen definida en el atributo poster
-  video1.src=null;
+  video1.muted = true;
+  video2.muted = false;
+  video3.muted = true;
+
+  video1.style.border = '4px solid black';
+  video2.style.border = '4px solid green';
+  video3.style.border = '4px solid black';
+
+  pantalla_general.play();
+};
+
+boton3.onclick = () => {
+  console.log("Video3");
+  pantalla_general.src= video3.src;
+  pantalla_general.currentTime = video3.currentTime;
+
+  video1.muted = true;
+  video2.muted = true;
+  video3.muted = false;
+
+  video1.style.border = '4px solid black';
+  video2.style.border = '4px solid black';
+  video3.style.border = '4px solid green';
+  condicion = false;
+
+  pantalla_general.play();
+};
+
+// Mejoras
+const automatico = document.getElementById("automatico");
+
+automatico.onclick = () => {
+  condicion = true;
+  while (condicion) {
+    console.log("hola");
+    wait(3000);
+  }
 }
